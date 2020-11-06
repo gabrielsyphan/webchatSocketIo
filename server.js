00d5a1ad8f00e702f60f68af5431d007bbbcc4b5
Gabriel Syphan <43450499+gabrielsyphan@users.noreply.github.com>
2020-11-06 16:02:52 -0300

    first commit

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
-const http = require("http").Server();
-const io = require("socket.io")(http);
+const express = require('express')
 
-io.on("connection", socket => {
-    socket.on("user-connected", user => {
-        socket.user = user;
+// https://expressjs.com/en/4x/api.html 
+const app = express()
+app.use(express.static("public"))
 
-        socket.broadcast.emit("users-changed", { user: user, event: "connected" });
-    });
+// https://nodejs.org/api/http.html
+const http = require('http').Server(app)
 
-    socket.on("message", data => {
-        io.emit("message", data);
-    });
+const serverSocket = require('socket.io')(http)
 
-    socket.on("disconnect", () => {
-        io.emit("users-changed", { user: socket.user, event: "disconnected" });
-    });
-});
+const porta = process.env.PORT || 8000
 
-let port = Number(process.env.PORT || 3000);
+const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
 
-http.listen(port, () => {
-    console.log("Listening on " + port);
-});
+http.listen(porta, () => {
+    const portaStr = porta === 80 ? '' :  ':' + porta
+    if (process.env.HEROKU_APP_NAME)
+        console.log('Servidor iniciado. Abra o navegador em ' + host)
+    else console.log('Servidor iniciado. Abra o navegador em ' + host + portaStr)
+})
+
+app.get('/', (requisicao, resposta) => resposta.sendFile(__dirname + '/index.html'))
+
+serverSocket.on('connect', recebeConexaoUsuario)
+
+function recebeConexaoUsuario(socket) {
+    socket.on('login', (nickname) => registraLoginUsuario(socket, nickname))
+    socket.on('disconnect', () => console.log('Cliente desconectado: ' + socket.nickname))
+    socket.on('chat msg', (msg) => encaminhaMsgsUsuarios(socket, msg))
+    socket.on('status', (msg) => encaminhaMsgStatus(socket, msg))
+}
+
+function encaminhaMsgStatus(socket, msg) {
+    console.log(msg)
+    socket.broadcast.emit('status', msg)
+}
+
+function encaminhaMsgsUsuarios(socket, msg) {
+    serverSocket.emit('chat msg', `${socket.nickname} diz: ${msg}`)
+}
+
+function registraLoginUsuario(socket, nickname) {
+    socket.nickname = nickname
+    const msg = nickname + ' conectou'
+    console.log(msg)
+    serverSocket.emit('chat msg', msg)
+}
