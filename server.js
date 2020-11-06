7d95d8521a09a77dbbce06bb77d07388b1c52b51
Gabriel Syphan <43450499+gabrielsyphan@users.noreply.github.com>
2020-11-06 20:44:27 -0300

    update server.js

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get('/', (requisicao, resposta) => resposta.sendFile(__dirname + '/index.htm
 serverSocket.on('connect', recebeConexaoUsuario)
 
 function recebeConexaoUsuario(socket) {
-    socket.on('login', (nickname) => registraLoginUsuario(socket, {nick: nickname, aux: 1}))
-    socket.on('disconnect', (nickname) => registraLoginUsuario(socket, {nick: nickname, aux: 2}))
+    socket.on('login', (nickname) => registraLoginUsuario(socket, nickname))
+    socket.on('disconnect', (nickname) => registraLoginUsuario(socket, nickname))
     socket.on('chat msg', (data) => encaminhaMsgsUsuarios(socket, data))
     socket.on('status', (msg) => encaminhaMsgStatus(socket, msg))
 }
@@ -40,12 +40,9 @@ function encaminhaMsgsUsuarios(socket, data) {
     serverSocket.emit('chat msg', {user: socket.nickname, date: data.date, msg: data.msg})
 }
 
-function registraLoginUsuario(socket, data) {
-    socket.nickname = data.nick
-    if(data.aux == 1 || data.aux == '1') {
-        const msg = nickname + ' conectou.';
-    } else {
-        const msg = nickname + ' desconectou.';
-    }
+function registraLoginUsuario(socket, nickname) {
+    socket.nickname = nickname
+    const msg = nickname // conectou
+    console.log(msg)
     serverSocket.emit('new user', msg)
 }
